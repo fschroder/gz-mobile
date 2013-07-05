92288f6bdac334e410f56fa75b779ff38d8ebd48
Guille Constantino <gconstantino@runway10.com>
2013-07-05 12:11:59 -0300

    Added basic form

---
+++ assets/www/scripts/data.js
@@ -0,0 +1,55 @@
+var serializeObject = function(element) {
+  var fields = $(element).serializeArray();
+  var object = {};
+  jQuery.each(fields, function(i, field) {
+    object[field.name] = field.value;
+  });
+  return object;
+};
+
+var showResult = function() {
+
+  var obj = serializeObject("form");
+
+  var gender = (obj['gender'] == 'male') ? 'Mr.' : 'Ms.';
+  var msg = '<p>Hello ' + gender + obj['name'] + '</p>';
+  msg += '<i>Your email is: ' + obj['email'] + '</i>';
+  msg += '<p>Message: ' + obj['needs'] + '</p>';
+
+  $("#dataResult").append(msg);
+  // jQuery.each(fields, function(i, field) {
+  // $("#dataResult").append(field.value + " ");
+  // });
+};
+
+var onDeviceReady = function() {
+  $("#data2").css('display', 'none');
+  $("#dataResult").css('display', 'none');
+
+  $("form").submit(function() {
+    return false;
+  });
+  var validator = $("#basicForm").validate({
+    rules : {
+      field : {
+        required : true
+      }
+    }
+  });
+
+  $("#submit1").click(function() {
+    if (validator.form()) {
+      $("#data1").css('display', 'none');
+      $("#data2").css('display', 'block');
+    }
+  });
+
+  $("#submit2").click(function() {
+    $("#data2").css('display', 'none');
+    $("#dataResult").css('display', 'block');
+    showResult();
+  });
+
+};
+
+$(document).ready(onDeviceReady);
